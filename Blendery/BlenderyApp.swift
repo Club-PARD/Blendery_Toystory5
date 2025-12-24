facf7a47980aa6504cc378a4b0a0ca31e25c26a8
parkyeongeon <yeongeon1003@gmail.com>
2025-12-24 15:25:20 +0900

    Initial Commit

---
+++ Blendery/BlenderyApp.swift
@@ -0,0 +1,17 @@
+//
+//  BlenderyApp.swift
+//  Blendery
+//
+//  Created by 박영언 on 12/24/25.
+//
+
+import SwiftUI
+
+@main
+struct BlenderyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BlenderyTests/BlenderyTests.swift
@@ -0,0 +1,16 @@
+//
+//  BlenderyTests.swift
+//  BlenderyTests
+//
+//  Created by 박영언 on 12/24/25.
+//
+
+import Testing
+
+struct BlenderyTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
