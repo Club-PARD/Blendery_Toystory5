0a81380fc4b47fff8ff2668a58a4e8fc8017bdb5
Sungjun <qkrtjdwns02818@gmail.com>
2026-01-03 02:07:44 +0900

    로그인 코드 주석 / 메인페이지 코드 리펙토링 전 커밋

---
+++ Blendery/MainPage_View/View/Toastmessage_View.swift
@@ -0,0 +1,18 @@
+//
+//  Toastmessage_View.swift
+//  Blendery
+//
+//  Created by 박성준 on 1/3/26.
+//
+
+import SwiftUI
+
+struct Toastmessage_View: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    Toastmessage_View()
+}
